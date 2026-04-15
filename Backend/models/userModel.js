7a32ba280eaee71f8f1eca70ef385ef516c253e3
shanbel <shanbeldires7@gmail.com>
2026-04-15 08:41:44 +0300

    add auth part

---
+++ Backend/models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["buyer","seller", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
---
+++ Backend/routes/authRoute.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+  login,
+  signup,
+  logOut,
+  getMe,
+  AccessRefreshToken,
+ 
+} from "../controllers/authController.js";
+import { verifyToken } from "../middleware/authentication.js";
+
+const authRoutes = express.Router();
+
+authRoutes.post("/login", login);
+authRoutes.post("/register", signup);
+authRoutes.post("/logout", verifyToken, logOut);
+authRoutes.post("/refresh", AccessRefreshToken);
+authRoutes.get("/profile", verifyToken, getMe);
+authRoutes.put("/profile", verifyToken, getMe);
+
+export default authRoutes;
