8605ad01407ccf84fbd9a1a532f3e48bc0da2806
Shanbel Dires <shanbeldires7@gmail.com>
2026-04-20 13:52:19 +0300

    add role based middleware

--- Backend/routes/sellerRoute.js
+++ Backend/routes/sellerRoute.js
@@ -2,11 +2,12 @@ import express from "express";
 const sellerRoutes = express.Router();
 import seller from "../controller/sellerController.js";
 import authenticateAccessToken from "../middleware/authentication.js";
+import authorize from "../middleware/autherization.js";
 
-sellerRoutes.post("/listings", authenticateAccessToken, seller.createListing);
-sellerRoutes.put("/listings/:id", authenticateAccessToken, seller.updateListing);
-sellerRoutes.delete("/listings/:id", authenticateAccessToken, seller.deleteListing);
-sellerRoutes.patch("/listings/:id/sold", authenticateAccessToken, seller.markAsSold);
-sellerRoutes.get("/orders", authenticateAccessToken, seller.getSellerOrders);
+sellerRoutes.post("/listings", authenticateAccessToken,  authorize("seller"),seller.createListing);
+sellerRoutes.put("/listings/:id", authenticateAccessToken,authorize("seller"), seller.updateListing);
+sellerRoutes.delete("/listings/:id", authenticateAccessToken, authorize("seller"),seller.deleteListing);
+sellerRoutes.patch("/listings/:id/sold", authenticateAccessToken,authorize("seller"), seller.markAsSold);
+sellerRoutes.get("/orders", authenticateAccessToken, authorize("seller"),seller.getSellerOrders);
 
 export default sellerRoutes;
