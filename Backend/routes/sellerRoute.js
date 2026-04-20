90d91f8a9e85285c9cd5bd0c4b80490211ee0f8d
Shanbel Dires <shanbeldires7@gmail.com>
2026-04-20 13:40:35 +0300

    change router name to sellerRoutes

--- Backend/routes/sellerRoute.js
+++ Backend/routes/sellerRoute.js
@@ -1,12 +1,12 @@
 import express from "express";
-const router = express.Router();
+const sellerRoutes = express.Router();
 import seller from "../controller/sellerController.js";
 import authenticateAccessToken from "../middleware/authentication.js";
 
-router.post("/listings", authenticateAccessToken, seller.createListing);
-router.put("/listings/:id", authenticateAccessToken, seller.updateListing);
-router.delete("/listings/:id", authenticateAccessToken, seller.deleteListing);
-router.patch("/listings/:id/sold", authenticateAccessToken, seller.markAsSold);
-router.get("/orders", authenticateAccessToken, seller.getSellerOrders);
+sellerRoutes.post("/listings", authenticateAccessToken, seller.createListing);
+sellerRoutes.put("/listings/:id", authenticateAccessToken, seller.updateListing);
+sellerRoutes.delete("/listings/:id", authenticateAccessToken, seller.deleteListing);
+sellerRoutes.patch("/listings/:id/sold", authenticateAccessToken, seller.markAsSold);
+sellerRoutes.get("/orders", authenticateAccessToken, seller.getSellerOrders);
 
-export default router;
+export default sellerRoutes;
