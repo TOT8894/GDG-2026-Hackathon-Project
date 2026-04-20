0a95ec3ff99332cff12467eba413f79fd68c8396
Shanbel Dires <shanbeldires7@gmail.com>
2026-04-20 13:49:49 +0300

    add role based middleware
    Refactor buyer routes to include authorization middleware for buyer actions.

--- Backend/routes/buyerRoute.js
+++ Backend/routes/buyerRoute.js
@@ -2,9 +2,9 @@ import express from "express";
 const buyerRoutes = express.Router();
 import buyer from "../controller/buyerController.js";
 import authenticateAccessToken from "../middleware/authentication.js";
-
-buyerRoutes.get("/listings", authenticateAccessToken, buyer.getListings);
-buyerRoutes.post("/orders/create", authenticateAccessToken, buyer.createOrder);
-buyerRoutes.post("/payments/initiate", authenticateAccessToken, buyer.initiatePayment);
+import authorize from "../middleware/autherization.js";
+buyerRoutes.get("/listings", authenticateAccessToken, authorize("buyer"), buyer.getListings);
+buyerRoutes.post("/orders/create", authenticateAccessToken, authorize("buyer"), buyer.createOrder);
+buyerRoutes.post("/payments/initiate", authenticateAccessToken, authorize("buyer"), buyer.initiatePayment);
 
 export default buyerRoutes;
