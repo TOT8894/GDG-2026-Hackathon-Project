5370204947fb12c45b3b0a613aeff55c208f6769
Shanbel Dires <shanbeldires7@gmail.com>
2026-04-20 13:43:07 +0300

    Rename router to dashboardRoutes in dashboardRoute.jsc

--- Backend/routes/dashboardRoute.js
+++ Backend/routes/dashboardRoute.js
@@ -1,8 +1,8 @@
 import express from "express";
-const router = express.Router();
+const dashboardRoutes = express.Router();
 import { getDashboard } from "../controller/dashboardController.js";
 import authenticateAccessToken from "../middleware/authentication.js";
 
-router.get("/", authenticateAccessToken, getDashboard);
+dashboardRoutes.get("/", authenticateAccessToken, getDashboard);
 
-export default router;
+export default dashboardRoutes;
